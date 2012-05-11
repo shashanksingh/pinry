67112c35390efa47eef87fd6cc516720b08636ee
shashank singh <shashank.personal@gmail.com>
2012-05-11 10:57:34 -0400

    added a PIL resizing code but commented it as it keep on throwing error "deocoder jpeg not found" looks like it doesnt like running on virtualenv

--- pinry/api/views.py
+++ pinry/api/views.py
@@ -6,6 +6,7 @@ from django.core.files.images import ImageFile
 from django.template.response import TemplateResponse
 import urllib2
 import hashlib
+import imageHandler
 
 def pins_recent(request, page=1):
     start_pin = abs(int(page) - 1) * 25
@@ -30,27 +31,18 @@ def pins_add_form(request):
     return HttpResponse("<html><body onload=\"window.resizeTo(600,600)\"><p><img src=\""+mediaBM+"\"/><br/><form action=\"/api/pins/add\"><input type=\"hidden\" name=\"media\" value=\""+mediaBM+"\" /><h2 style=\"font: normal 18px 'Lobster', cursive , bold; \">Describe what you like?</h2><br/><input type=\"textarea\" value=\"Like\" name=\"description\"style=\"width:200px; height: 40px;\"/><br/><br/><input type=\"submit\" style=\"background-color:#DFDFDF; \"/></form></p></body></html>")
     #return TemplateResponse(request, 'pins/new_pin_bookmarklet.html')
 
-#FIXME: i am full of dirty hacks :(
 def pins_add(request):
     mediaBM=str(request.GET.get('media'))
     #urlBM=str(request.GET.get('url'))
     descriptionBM=str(request.GET.get('description'))
     #titleBM=str(request.GET.get('title'))
     #is_videoBM=request.GET.get('is_video')
-    nameHash = hashlib.sha224(mediaBM.split('/')[-1])
-    nameOfMedia = "pins/pin/" + nameHash.hexdigest() + '.jpg'
+    nameHash = hashlib.sha224(mediaBM.split('/')[-1]).hexdigest()
+    nameOfMedia = "pins/pin/" + nameHash + '.jpg'
     pinNew = Pin(url=mediaBM,description=descriptionBM,image=nameOfMedia)
-    f_orig=open("/opt/sources/code/pinry/media/"+nameOfMedia,'w')
-    f_thumbnail=open("/opt/sources/code/pinry/media/"+nameOfMedia.rstrip('.jpg')+".200x1000.jpg",'w')
-    img_temp = ImageFile(f_orig)
-    img_thumb_temp = ImageFile(f_thumbnail)
-    temp_image = urllib2.urlopen(mediaBM).read()
-    img_temp.write(temp_image)
-    img_thumb_temp.write(temp_image)
-    img_temp.flush()
-    img_thumb_temp.flush()
-    img_temp.close()
-    img_thumb_temp.close()
-    #img_temp.save(nameOfMedia)
+    rawImage = imageHandler.downloadImage(mediaBM)
+    imageHandler.saveImage(rawImage,nameHash)
+    imageHandler.makeThumbnail(rawImage,nameHash)
+    #imageHandler.makeThumbnail(nameHash)
     pinNew.save()
     return HttpResponse("<html><body onload=\"window.resizeTo(600,600)\"><p> <h2 style=\"font: normal 26px 'Lobster', cursive , bold;\" >Image Succesfully Loaded</h2><br/><img src=\""+mediaBM+"\"/><br/></p></body></html>")

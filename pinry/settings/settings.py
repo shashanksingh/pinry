c39c5f93e729015422dcaf3b1a450cc21d0eb2b8
shashank singh <shashank.personal@gmail.com>
2012-05-16 15:07:34 +0530

    added google analytics code tracking in settings.py

--- pinry/settings/settings.py
+++ pinry/settings/settings.py
@@ -2,4 +2,6 @@ MEDIA_DIR = "/home/shanky/code/pinry/media/pins/pin"
 BRANDING_LOGO = "http://www.getthelook.in/media/images/logo_old.png"
 BRANDING_LINK = "http://www.getthelook.in"
 APP_NAME = "TrendBoard"
+GOOGLE_ANALYTICS_CODE = "UA-27971145-1"
+DOMAIN_NAME = "getthelook.in"
 

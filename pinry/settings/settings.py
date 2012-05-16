4fe3a304dfc3cf8845b026ff9114c034ec78c9f5
shashank singh <shashank.personal@gmail.com>
2012-05-16 14:50:29 +0530

    fixed logo placement

--- pinry/settings/settings.py
+++ pinry/settings/settings.py
@@ -1,5 +1,5 @@
 MEDIA_DIR = "/home/shanky/code/pinry/media/pins/pin"
-BRANDING_LOGO = "http://www.getthelook.in/media/theme/image/logo.png"
+BRANDING_LOGO = "http://www.getthelook.in/media/images/logo_old.png"
 BRANDING_LINK = "http://www.getthelook.in"
 APP_NAME = "TrendBoard"
 

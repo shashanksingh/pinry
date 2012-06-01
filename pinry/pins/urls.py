44e57a7fcc220c33139da195a3b028ad2e2eeded
shashank singh <shashank.personal@gmail.com>
2012-06-01 14:31:43 +0530

    added a tempelate for user specific pins

--- pinry/pins/urls.py
+++ pinry/pins/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('pinry.pins.views',
     url(r'^$', 'recent_pins', name='recent-pins'),
     url(r'^new-pin/$', 'new_pin', name='new-pin'),
+    url(r'^user-pin/$','user_pins', name='user-pin'),
 )
